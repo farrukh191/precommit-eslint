be9fd7fdd23d4dff7de663249ab43fb4267cce01
@mfarrukh1 <mfarrukh@nisum.com>
2024-02-13 23:00:29 +0500

    df

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 function App() {
+  console.log()
   return (
-  <div>
-         <h1>Setup Eslint, prettier and git husky</h1>
+      <div>
+            <h1>Setup Eslint, prettier and git husky</h1>
   </div>
   );
 }
