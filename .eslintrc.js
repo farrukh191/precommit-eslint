cca74bb4de6aa10912485239e434b653c26e72b2
@mfarrukh1 <mfarrukh@nisum.com>
2024-02-13 22:21:38 +0500

    now

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+
+module.exports = {
+    extends: ["react-app", "react-app/jest"],
+    rules: {
+      "no-console": "error",
+      "no-unused-vars": ["error", { args: "none" }],
+    },
+  };
+  
