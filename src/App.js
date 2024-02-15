30ca24d96987e5f5f9faa1e8c4696a51390b1243
@mfarrukh1 <mfarrukh@nisum.com>
2024-02-15 22:07:48 +0500

    now it should work

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 function App() {
   console.log();
   let as;
+  let as2;
+  console.log("");
   return (
-            <div>
+    <                       div>
       <h1>Setup Eslint, prettier and git husky</h1>
+                <h1>Setup Eslint, prettier and git husky</h1>
     </div>
   );
 }
