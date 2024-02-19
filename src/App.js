547385555e88d4d0f1f439022f4fbaffd551c4d7
@mfarrukh1 <mfarrukh@nisum.com>
2024-02-19 21:53:33 +0500

    Implementing and testing new feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 
 function App() {
-  console.log("");
+  var a;
   return (
-    <div>
-      <h1>Setup Eslint, prettier and git husky</h1>
-      <h1>Setup Eslint, prettier and git husky</h1>
+                      <div>
+               <h1>Setup Eslint, prettier and git husky</h1>
     </div>
   );
 }
